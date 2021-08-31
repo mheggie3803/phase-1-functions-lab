31977322d47c11822a72e61427c62c064355d41b
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-08-31 10:15:38 -0400

    commit tree cf9011c65c2dbf10868574a671e7afe38be0194b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+function distanceFromHqInBlocks(someValue) {
+    if(someValue <= 42 ) {
+        return 42 - someValue;
+    } else {
+        return someValue - 42;
+    }
+}
+
+
+
+
+//return a distance in feet
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264 
+}
+
+
+
+function distanceTravelledInFeet(start, ending) {
+    if(someValue <= 50 ) {
+        return 
+    }
+}
